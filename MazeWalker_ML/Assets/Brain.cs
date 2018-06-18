7730a164230edeafd9812588eddd802fa67086d0
AkashKumarAM <32879402+AkashKumarAM@users.noreply.github.com>
2018-06-18 17:29:11 -0500

    Add files via upload

---
+++ MazeWalker_ML/Assets/Brain.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    private int DNALength = 2;
+    public DNA dna;
+    public GameObject eyes;
+    private bool seeWall = true;
+    Vector3 startingPos;
+    public float distanceTravelled;
+    private bool alive = true;
+
+
+
+
+    public void InIt()
+    {
+        // initialise DNA
+        //0 forward
+        //1 turn 
+
+        dna = new DNA(DNALength, 360);
+        startingPos = this.transform.position;
+        alive = true;
+
+    }
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.tag == "dead")
+        {
+            alive = false;
+            distanceTravelled = 0;
+        }
+    }
+
+
+   
+
+    public void Update()
+    {
+        if (!alive) return;
+
+        //Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 10, Color.red, 10);
+        seeWall = false;
+        RaycastHit hit;
+        if (Physics.SphereCast(eyes.transform.position, 0.1f, eyes.transform.forward, out hit, 0.5f))
+        {
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                seeWall = true;
+            }
+        }
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!alive) return;
+
+        //read DNA
+
+        float h = 0;
+        float v = dna.GetGene(0);
+
+        if (seeWall)
+        {
+            h = dna.GetGene(1);
+        }
+
+        this.transform.Translate(0,0,v*0.001f);
+        this.transform.Rotate(0,h,0);
+        distanceTravelled = Vector3.Distance(startingPos, this.transform.position); 
+
+
+
+    }
+}
---
+++ MazeWalker_ML/Assets/PopulationManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour
+{
+
+    public GameObject botPrefab;
+    public GameObject startingPos;
+    public int populationSize = 50;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public float trailtime = 5;
+    private int generation = 1;
+
+
+    GUIStyle guiStyle = new GUIStyle();
+
+    void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 200, 30), "GEN" + generation, guiStyle);
+        GUI.Label(new Rect(10, 50, 200, 30), String.Format("TIME:{0:0.00}", elapsed), guiStyle);
+        GUI.Label(new Rect(10, 75, 200, 30), "Population" + population.Count, guiStyle);
+        GUI.EndGroup();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            
+            GameObject b = Instantiate(botPrefab, startingPos.transform.position, this.transform.rotation);
+            b.GetComponent<Brain>().InIt();
+            population.Add(b);
+
+        }
+
+    }
+
+
+    GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        
+
+        GameObject offSpring = Instantiate(botPrefab, startingPos.transform.position, this.transform.rotation);
+        Brain b = offSpring.GetComponent<Brain>();
+        if (UnityEngine.Random.Range(0, 100) == 1)
+        {
+            b.InIt();
+            b.dna.Mutate();
+        }
+        else
+        {
+            b.InIt();
+            b.dna.Combine(parent1.GetComponent<Brain>().dna, parent2.GetComponent<Brain>().dna);
+
+        }
+
+        return offSpring;
+    }
+
+    void BreedNewPopulation()
+    {
+        List<GameObject> sortedlist = population.OrderBy(o => o.GetComponent<Brain>().distanceTravelled).ToList();
+        population.Clear();
+        for (int i = (int)(sortedlist.Count / 2.0f) - 1; i < sortedlist.Count - 1; i++)
+        {
+            population.Add(Breed(sortedlist[i], sortedlist[i + 1]));
+            population.Add(Breed(sortedlist[i + 1], sortedlist[i]));
+
+        }
+
+        for (int i = 0; i < sortedlist.Count; i++)
+        {
+            Destroy(sortedlist[i]);
+        }
+
+        generation++;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        elapsed += Time.deltaTime;
+        if (elapsed >= trailtime)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+
+
+
+    }
+}
